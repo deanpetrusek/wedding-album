43b32a7352f00742d3c0ba26a62958f336d3e315
Ronald Petrusek <rdpetrusek@ronalds-mbp.lan>
2017-12-23 21:56:54 -0600

    removing the test stuff

--- Async/Startup.cs
+++ Async/Startup.cs
@@ -42,7 +42,7 @@ namespace Async
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Test}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
